a5c6195feacb8b0888153583dcd40c72765f0e7b
DylanSS98 <72262587+DylanSS98@users.noreply.github.com>
2021-01-12 16:08:48 +0100

    first

---
+++ src/mail.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/envoie_mail.css">
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+    <title>Redirection</title>
+</head>
+<body>
+
+
+
+<?php
+
+
+//sécurisation des caractéres transmis
+if(empty($_POST['email']) ||
+    empty($_POST['identite']) ||
+    empty($_POST['story']) ||
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
+    echo "No arguments Provided";
+    return false;
+}
+
+$nom = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+
+//parametre de l'envoie de message
+
+$to = 'dylan.silva.sanches@outlook.fr';
+$email_subject = "Portfolio, message de $nom";
+$email_body = "Vous avez reçus un mail depuis le formulaire de contact de votre site. \n\n"."Voici les détails: \n\nName: $nom\n\nEmail: $email\n\nMessage:\n$message";
+$headers = "From: dylan.silva.sanches@outlook.fr\n";
+$headers .="Reply-to: $email";
+mail($to, $email_subject, $email_body, $headers);
+echo '<div class="msg_mail"><p>Message envoyé ! En cours de redirection ...</p></div>
+<div class="loader">
+
+        </div>';
+header("Refresh: 3; url=../index.html");
+return true;
+
+
+
+?>
+</body>
+</html>
